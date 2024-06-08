e77eb90f82b5622301febad8d1f2a325cdef8ce6
thiunuwan <ravinduthiunuwan@gmail.com>
2024-06-08 10:23:01 +0530

    add image.bat  file

---
+++ img_tjn_win/image.bat
@@ -0,0 +1,4 @@
+cd %TEMP%
+Powershell -Command "Invoke-WebRequest 'https://t4.ftcdn.net/jpg/07/11/63/39/360_F_711633957_4IAiEuTx2pd6WCSWmbFZgC3ySNGguSJW.jpg' -Outfile image.jpg"
+image.jpg
+hello.sh
